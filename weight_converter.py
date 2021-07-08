4ab5b7c11b8b3936899b4b3e0c7cf0d739910cc3
Monica <monica@hackbrightacademy.com>
2021-07-08 05:00:19 +0000

    created a weight converter, asks users for weight and converts to lbs or kg

---
+++ weight_converter.py
@@ -0,0 +1,10 @@
+weight = input('weight: ')
+weight = int(weight)
+unit = input('(L)bs or (K)g: ')
+
+if unit.upper()  == 'L':
+    converted = weight*0.45
+    print (f'you are {converted} kilos')
+else:
+    converted = weight/0.45
+    print (f'you are {converted} pounds')
